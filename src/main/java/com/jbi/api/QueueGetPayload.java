b0304cd8cadfcecb6501453535dad3414e4a1d1b
JMit-dev <darkkid8192004@gmail.com>
2025-06-11 16:34:11 -0400

    minor package format mistake

--- src/main/java/com/jbi/api/QueueGetPayload.java
+++ src/main/java/com/jbi/api/QueueGetPayload.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
 public record QueueGetPayload(
-        java.util.List<QueueItem> queue,
+        List<QueueItem> queue,
 
         @JsonProperty("running_item")  QueueItem runningItem,
         @JsonProperty("plan_queue_uid")   String planQueueUid,
