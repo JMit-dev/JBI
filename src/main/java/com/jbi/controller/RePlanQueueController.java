4a4c2f2a990a1bb21f3225911747fd48da2370aa
JMit-dev <darkkid8192004@gmail.com>
2025-06-12 10:23:26 -0400

    feat: RePlanQUeue widget

---
+++ src/main/java/com/jbi/controller/RePlanQueueController.java
@@ -0,0 +1,59 @@
+package com.jbi.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleButton;
+
+public class RePlanQueueController {
+
+    @FXML
+    private Button bottomBtn;
+
+    @FXML
+    private Button clearBtn;
+
+    @FXML
+    private Button deleteBtn;
+
+    @FXML
+    private Button deselectBtn;
+
+    @FXML
+    private Button downBtn;
+
+    @FXML
+    private Button duplicateBtn;
+
+    @FXML
+    private TableColumn<?, ?> grpCol;
+
+    @FXML
+    private TableColumn<?, ?> idxCol;
+
+    @FXML
+    private ToggleButton loopBtn;
+
+    @FXML
+    private TableColumn<?, ?> paramCol;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    private Button topBtn;
+
+    @FXML
+    private TableColumn<?, ?> typeCol;
+
+    @FXML
+    private TableColumn<?, ?> typeCol1;
+
+    @FXML
+    private Button upBtn;
+
+    @FXML
+    private TableColumn<?, ?> userCol;
+
+}
