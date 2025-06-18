fea51920ffe6813de10ab7c1a8ba8bdcc9ba8b4e
JMit-dev <darkkid8192004@gmail.com>
2025-06-18 11:43:20 -0400

    feat: ReQueueControls widget and skeleton controller

---
+++ src/main/java/com/jbi/controller/ReQueueControlsController.java
@@ -0,0 +1,38 @@
+package com.jbi.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+
+public class ReQueueControlsController {
+
+    @FXML
+    private CheckBox autoChk;
+
+    @FXML
+    private Label reStatusLabel;
+
+    @FXML
+    private Button startBtn;
+
+    @FXML
+    private Button stopBtn;
+
+    @FXML
+    void auto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void start(ActionEvent event) {
+
+    }
+
+    @FXML
+    void stop(ActionEvent event) {
+
+    }
+
+}
