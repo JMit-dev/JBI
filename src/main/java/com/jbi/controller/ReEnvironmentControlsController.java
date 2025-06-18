0b8601dff6a4a694475501fd17ee528af265776c
JMit-dev <darkkid8192004@gmail.com>
2025-06-18 11:30:27 -0400

    feat: environment controls widget + controller skeleton

---
+++ src/main/java/com/jbi/controller/ReEnvironmentControlsController.java
@@ -0,0 +1,33 @@
+package com.jbi.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ReEnvironmentControlsController {
+
+    @FXML
+    private Button OpenBtn;
+
+    @FXML
+    private Button closeBtn;
+
+    @FXML
+    private Button destroyBtn;
+
+    @FXML
+    void close(ActionEvent event) {
+
+    }
+
+    @FXML
+    void destroy(ActionEvent event) {
+
+    }
+
+    @FXML
+    void open(ActionEvent event) {
+
+    }
+
+}
