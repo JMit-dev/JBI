ab21f790a87c35c1babf2d54c4fa0caed49d00c1
JMit-dev <darkkid8192004@gmail.com>
2025-06-18 14:40:26 -0400

    feat: ReExecutionControls widget and controller skeleton

---
+++ src/main/java/com/jbi/controller/ReExecutionControlsController.java
@@ -0,0 +1,65 @@
+package com.jbi.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ReExecutionControlsController {
+
+    @FXML
+    private Button abortBtn;
+
+    @FXML
+    private Button ctrlcBtn;
+
+    @FXML
+    private Button haltBtn;
+
+    @FXML
+    private Button pauseDefBtn;
+
+    @FXML
+    private Button pauseImmBtn;
+
+    @FXML
+    private Button resumeBtn;
+
+    @FXML
+    private Button stopBtn;
+
+    @FXML
+    void abort(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ctrlc(ActionEvent event) {
+
+    }
+
+    @FXML
+    void halt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pauseDeffered(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pauseImmediate(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resume(ActionEvent event) {
+
+    }
+
+    @FXML
+    void stop(ActionEvent event) {
+
+    }
+
+}
