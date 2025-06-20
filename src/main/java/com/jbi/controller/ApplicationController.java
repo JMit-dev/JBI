9f2714216e36e1d5d22bd65489ff8d9e767d5286
JMit-dev <darkkid8192004@gmail.com>
2025-06-20 14:57:39 -0400

    Application and application controller

--- src/main/java/com/jbi/controller/ApplicationController.java
+++ src/main/java/com/jbi/controller/ApplicationController.java
@@ -1,4 +1,30 @@
 package com.jbi.controller;
 
-public class ApplicationController {
+import com.jbi.view.ViewFactory;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Tab;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public final class ApplicationController implements Initializable {
+
+    @FXML private CheckBox activateDestroyChk;
+    @FXML private Tab      monitorQueueTab;
+    @FXML private Tab      editAndControlQueueTab;
+    @FXML private MenuItem savePlanTxt, savePlanJson, savePlanYaml;
+
+    @Override public void initialize(URL url, ResourceBundle rb) {
+        monitorQueueTab.setContent(ViewFactory.MONITOR_QUEUE.get());
+        editAndControlQueueTab.setContent(ViewFactory.EDIT_AND_CONTROL_QUEUE.get());
+    }
+
+    /* -------- menu skeletons – wire up later ------------------------- */
+    @FXML private void toggleDestroyActivation() { /* TODO */ }
+    @FXML private void savePlanTxt()             { /* TODO */ }
+    @FXML private void savePlanJson()            { /* TODO */ }
+    @FXML private void savePlanYaml()            { /* TODO */ }
 }
