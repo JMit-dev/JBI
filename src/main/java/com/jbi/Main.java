76df21c91368f56c094f7c9d342d32d448326ab4
JMit-dev <darkkid8192004@gmail.com>
2025-06-20 14:56:52 -0400

    feat: view factory

--- src/main/java/com/jbi/Main.java
+++ src/main/java/com/jbi/Main.java
@@ -1,38 +1,35 @@
 package com.jbi;
 
 import com.jbi.client.RunEngineHttpClient;
+import com.jbi.view.ViewFactory;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage stage) throws Exception {
+    @Override public void start(Stage stage) throws Exception {
+
         String baseUrl = "http://localhost:60610";
-        String apiKey = System.getenv("BLUESKY_API_KEY");
+        String apiKey  = System.getenv("BLUESKY_API_KEY");
         if (apiKey == null) {
             System.err.println("BLUESKY_API_KEY environment variable is not set.");
+            Platform.exit();
             return;
         }
 
-        RunEngineHttpClient.initialize(baseUrl, apiKey);  // singleton init
+        RunEngineHttpClient.initialize(baseUrl, apiKey);
 
-        Parent root = FXMLLoader.load(getClass().getResource("/view/MonitorQueue.fxml"));
+        Parent root = ViewFactory.APPLICATION.get();
         stage.setScene(new Scene(root));
         stage.setTitle("Queue Monitor");
         stage.setMinWidth(1010);
         stage.setMinHeight(710);
-        stage.setOnCloseRequest((w) -> Platform.exit());
+        stage.setOnCloseRequest(e -> Platform.exit());
         stage.show();
     }
 
-
-
-    public static void main(String[] args) {
-        launch(args);
-    }
+    public static void main(String[] args) { launch(args); }
 }
---
+++ src/main/java/com/jbi/controller/ApplicationController.java
@@ -0,0 +1,4 @@
+package com.jbi.controller;
+
+public class ApplicationController {
+}
